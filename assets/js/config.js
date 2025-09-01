9378666874dfb742e04f9c7e474749d1e7b627fa
valentyna-lyt <valentynaf2.403@gmail.com>
2025-09-01 23:06:56 +0300

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://tageqrvymxpjpziiejqq.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhZ2VxcnZ5bXhwanB6aWllanFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3NTY0NzMsImV4cCI6MjA3MjMzMjQ3M30.agzFthMi92LdAH2q6wPU2ItpwrIiHrjBg7sP5vLrhNY"
+};
